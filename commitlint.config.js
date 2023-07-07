513874f5741b859025b76a7f53a58bdbd44677fe
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-07-07 14:34:21 -0400

    chore: add commitlint and husky

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+const Configuration = {
+  extends: [`@commitlint/config-conventional`],
+  ignores: [(message) => message.includes(`Release`)],
+  rules: {
+    "type-enum": [
+      2,
+      `always`,
+      [
+        `feat`,
+        `fix`,
+        `docs`,
+        `style`,
+        `refactor`,
+        `perf`,
+        `tests`,
+        `chore`,
+        `revert`,
+      ],
+    ],
+  },
+};
+
+module.exports = Configuration;
