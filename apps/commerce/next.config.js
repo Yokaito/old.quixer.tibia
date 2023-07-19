ae07854b445c52c6cac59054a30b1bfa5dd79ead
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-07-18 21:24:06 -0400

    chore(commerce): add file tracing root to attempt to remove error in vercel deploy

--- apps/commerce/next.config.js
+++ apps/commerce/next.config.js
@@ -1,4 +1,10 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+}
 
 module.exports = nextConfig
