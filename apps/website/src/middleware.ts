8663279cfb9afa54bc39d7392fcdf1debf5e12ca
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-08-05 21:08:16 -0400

    feat: add seo information

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -20,5 +20,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
 }
