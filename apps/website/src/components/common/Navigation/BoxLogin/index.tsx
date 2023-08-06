ee1f4b4f199978df423db8c0db4784e3bacf9a76
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-08-05 20:51:33 -0400

    feat: add logo artwork above box login

--- apps/website/src/components/common/Navigation/BoxLogin/index.tsx
+++ apps/website/src/components/common/Navigation/BoxLogin/index.tsx
@@ -2,10 +2,15 @@ import { MenuBox } from '@/components/ui'
 import Button from '@/components/ui/Button'
 import styles from './box-login.module.scss'
 import Link from 'next/link'
+import Image from 'next/image'
+import LogoTibiaArtWork from '@/assets/images/logo/tibia-logo-artwork-top.webp'
 
 export const BoxLogin = () => {
   return (
-    <div className={`${styles.qxBoxLogin}`}>
+    <div className={`${styles.qxBoxLogin}`} data-qx-box-login>
+      <Link data-qx-logo-artwork href="/">
+        <Image src={LogoTibiaArtWork} alt="logo tibia artwork" />
+      </Link>
       <MenuBox>
         <Button>Login</Button>
         <Link
