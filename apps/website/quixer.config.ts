f8745b3fca5ed95267c6ee042e754649c613ca1f
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-08-29 20:39:26 -0400

    chore(website): remove server config in otConfig

--- apps/website/quixer.config.ts
+++ apps/website/quixer.config.ts
@@ -1,14 +1,6 @@
 export const otConfig = {
   server: {
-    worldName: `OTServBR-Global`,
-    ip: `127.0.0.1`,
-    loginProtocolPort: 7171,
-    gameProtocolPort: 7172,
-    pvpType: 1, // 0 = no-pvp, 1 = pvp, 2 = retro pvp
-    worldid: 0,
     premiumIsFree: true,
-    location: `BRA`,
-    battleEyeActive: false,
   },
   vocations: [
     { id: 0, name: `No Vocation` },
