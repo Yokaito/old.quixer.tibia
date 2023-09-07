c80a80f14543f540bdaacd1c1cf46c0956108b3c
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-09-07 11:16:50 -0400

    chore(website): using next_public_vercel_url

--- apps/website/src/sdk/lib/trpc/server.ts
+++ apps/website/src/sdk/lib/trpc/server.ts
@@ -1,5 +1,5 @@
 import { httpBatchLink } from '@trpc/client'
-
+import env from '@/sdk/env'
 import appRouter from '@/sdk/server/routers/_app'
 
 export const serverClient = appRouter.createCaller({
@@ -7,7 +7,10 @@ export const serverClient = appRouter.createCaller({
   links: [
     httpBatchLink({
       // eslint-disable-next-line turbo/no-undeclared-env-vars
-      url: `${process.env.NEXT_PUBLIC_URL}/api/trpc`,
+      url:
+        env.NODE_ENV === 'development'
+          ? `http://${env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+          : `https://${env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`,
     }),
   ],
 })
--- apps/website/src/sdk/providers/trpc/index.tsx
+++ apps/website/src/sdk/providers/trpc/index.tsx
@@ -4,6 +4,7 @@ import { httpBatchLink, loggerLink } from '@trpc/client'
 import React, { useState } from 'react'
 
 import { trpc } from '@/sdk/lib/trpc/client'
+import env from '@/sdk/env'
 
 export function TRPCProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -20,7 +21,10 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
         }),
         httpBatchLink({
           // eslint-disable-next-line turbo/no-undeclared-env-vars
-          url: `${process.env.NEXT_PUBLIC_URL}/api/trpc`,
+          url:
+            env.NODE_ENV === 'development'
+              ? `http://${env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+              : `https://${env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`,
         }),
       ],
     })
